0d0ca153cdfe0ef4481a4f3f68c5e10c0e643e6a
TokarevAleksander <84794987+TokarevAleksander@users.noreply.github.com>
2024-04-16 01:26:28 +0500

    Update task3.java

--- task3.java
+++ task3.java
@@ -1,28 +1,29 @@
 import java.util.Random;
 
-public class ArrayManipulation {
+public class task_3 {
 
     public static void main(String[] args) {
         int[] array = new int[20];
-        
+
         // Заполнение массива случайными числами от -10 до 10
         fillArray(array);
-        
+
         // Вывод исходного массива
         System.out.println("Исходный массив:");
         printArray(array);
-        
+
         // Поиск максимального отрицательного и минимального положительного элементов
         int maxNegative = findMaxNegative(array);
         int minPositive = findMinPositive(array);
-        
+
         // Вывод максимального отрицательного и минимального положительного элементов
         System.out.println("Максимальный отрицательный элемент: " + maxNegative);
         System.out.println("Минимальный положительный элемент: " + minPositive);
-        
-        // Поменять местами максимальный отрицательный и минимальный положительный элементы
+
+        // Поменять местами максимальный отрицательный и минимальный положительный
+        // элементы
         swapMaxNegativeAndMinPositive(array);
-        
+
         // Вывод измененного массива
         System.out.println("Измененный массив:");
         printArray(array);
@@ -39,23 +40,27 @@ public class ArrayManipulation {
     // Метод для поиска максимального отрицательного элемента в массиве
     public static int findMaxNegative(int[] array) {
         int maxNegative = Integer.MIN_VALUE;
+        boolean foundNegative = false;
         for (int num : array) {
             if (num < 0 && num > maxNegative) {
                 maxNegative = num;
+                foundNegative = true;
             }
         }
-        return maxNegative;
+        return foundNegative ? maxNegative : 0; // Возвращаем 0, если не найдено отрицательных чисел
     }
 
     // Метод для поиска минимального положительного элемента в массиве
     public static int findMinPositive(int[] array) {
         int minPositive = Integer.MAX_VALUE;
+        boolean foundPositive = false;
         for (int num : array) {
-            if (num >= 0 && num < minPositive) {
+            if (num > 0 && num < minPositive) {
                 minPositive = num;
+                foundPositive = true;
             }
         }
-        return minPositive;
+        return foundPositive ? minPositive : 0; // Возвращаем 0, если не найдено положительных чисел
     }
 
     // Метод для вывода массива на экран
@@ -65,12 +70,13 @@ public class ArrayManipulation {
         }
         System.out.println();
     }
-    
-    // Метод для обмена местами максимального отрицательного и минимального положительного элементов
+
+    // Метод для обмена местами максимального отрицательного и минимального
+    // положительного элементов
     public static void swapMaxNegativeAndMinPositive(int[] array) {
         int maxNegativeIndex = -1;
         int minPositiveIndex = -1;
-        
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 0 && array[i] == findMaxNegative(array)) {
                 maxNegativeIndex = i;
@@ -79,7 +85,7 @@ public class ArrayManipulation {
                 minPositiveIndex = i;
             }
         }
-        
+
         if (maxNegativeIndex != -1 && minPositiveIndex != -1) {
             int temp = array[maxNegativeIndex];
             array[maxNegativeIndex] = array[minPositiveIndex];
